6a7ad3ac275fc71cbb55557428cfdcc724786e57
yoolee song <loveyool212@gmail.com>
2018-03-24 03:06:23 +0900

    기초 벡터 실습1

---
+++ vector_basis.R
@@ -0,0 +1,20 @@
+a <- c(1,2,3,4,5)
+a[1]
+a[5]
+a[a>3]
+sum(a[a>1])
+sum(a>1)
+a>4
+a>1
+a[a>1]
+2+3+4+5
+which(a[a>4])
+which(a>4)
+which(a>2)
+b <- c(5,20,10,3,8,5,9)
+which(b>3)
+which(b>10)
+indexOfmax <- which.max(b)
+indexOfmax
+indexOfmin <- which.min(b)
+indexOfmin
